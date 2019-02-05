1f51ba0d489ab2d87179da915efc237d3ae6f612
Mikyas Ayele <mayele23@Mikyass-MBP.PK5001Z>
2019-02-05 03:00:12 -0800

    week 3 js assignments completed

---
+++ regular-challenge-1.js
@@ -0,0 +1,8 @@
+let names = ['mikyas0', 'mikyas1', 'mikyas2', 'mikyas3', 'mikyas4'];
+names.push('mikyas5');
+
+names.splice(3, 1);
+console.log(names);
+
+str = names.join(',');
+console.log(str);
---
+++ regular-challenge-2.js
@@ -0,0 +1,4 @@
+let arr= ['I', 'love', 'Manchester', 'United.'];
+str = arr.join(' ');
+console.log(str);
+console.log(arr[2]);
